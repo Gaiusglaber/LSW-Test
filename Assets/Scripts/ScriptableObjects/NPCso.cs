6f5309ec861d565ccd69d9d733cd04e79c8b1f7b
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-11-10 21:03:52 -0300

    Created animation files.

---
+++ Assets/Scripts/ScriptableObjects/NPCso.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPC", menuName = "NPCs")]
+public class NPCSO : ScriptableObject
+{
+    public enum NPCTYPES
+    {
+        FEET,
+        LEG,
+        CHEST
+    }
+    public NPCTYPES npcType;
+    public RuntimeAnimatorController animatorController = null;
+    public int ID = 0;
+    public Sprite chest = null;
+    public Sprite legs = null;
+    public Sprite feet = null;
+
+}
