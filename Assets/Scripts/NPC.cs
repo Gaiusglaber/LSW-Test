5eb5891959d65a0fd68ae226159a59e0f157fd38
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-11-11 18:12:16 -0300

    Quest signs added.

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField] private GameObject questObject = null;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            questObject.SetActive(true);
+        }
+
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            questObject.SetActive(false);
+        }
+    }
+}
