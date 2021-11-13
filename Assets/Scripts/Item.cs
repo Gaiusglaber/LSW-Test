3d9597785886a29e29b224cdae937eaf7f732e3e
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-11-12 22:34:25 -0300

    Hotfixed item size.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Item : MonoBehaviour
 {
+    public enum TYPE
+    {
+        FEET,
+        LEG,
+        CHEST,
+        HAIR
+    }
     #region EXPOSED_FIELDS
     [SerializeField] public TMPro.TMP_Text coinText = null;
     [SerializeField] public Image clothImage = null;
     [SerializeField] public TMPro.TMP_Text Title = null;
+    [NonSerialized] public TYPE ItemType;
+    [NonSerialized] public AnimatorOverrideController AnimatorController = null;
     #endregion
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -14,8 +14,12 @@ public class UIManager : MonoBehaviour
     [SerializeField] private List<Item> items = new List<Item>();
     [SerializeField] private float ImageSize = 0;
     #endregion
+    #region PRIVATE_FIELDS
+    private float InitialImageSize = 0;
+    #endregion
     private void Awake()
     {
+        InitialImageSize = ImageSize;
         foreach (var item in items)
         {
             
@@ -43,6 +47,26 @@ public class UIManager : MonoBehaviour
     {
         for (short i = 0; i < NPCList.Count; i++)
         {
+            items[i].ItemType = (Item.TYPE)NPCList[i].type;
+            switch (items[i].ItemType)
+            {
+                case Item.TYPE.CHEST:
+                    items[i].clothImage.rectTransform.anchoredPosition = new Vector2(items[i].clothImage.rectTransform.anchoredPosition.x, -20);
+                    ImageSize = InitialImageSize;
+                    break;
+                case Item.TYPE.LEG:
+                    items[i].clothImage.rectTransform.anchoredPosition = new Vector2(items[i].clothImage.rectTransform.anchoredPosition.x, 10);
+                    ImageSize = InitialImageSize;
+                    break;
+                case Item.TYPE.FEET:
+                    items[i].clothImage.rectTransform.anchoredPosition = new Vector2(items[i].clothImage.rectTransform.anchoredPosition.x, 20);
+                    ImageSize = InitialImageSize;
+                    break;
+                case Item.TYPE.HAIR:
+                    items[i].clothImage.rectTransform.anchoredPosition = new Vector2(items[i].clothImage.rectTransform.anchoredPosition.x, -30);
+                    ImageSize /= 1.2f;
+                    break;
+            }
             items[i].clothImage.sprite = NPCList[i].image;
             items[i].clothImage = IncreaseImageSize(items[i].clothImage);
             items[i].coinText.text = NPCList[i].price.ToString();
@@ -51,7 +75,7 @@ public class UIManager : MonoBehaviour
     }
     Image IncreaseImageSize(Image ImageToIncrease)
     {
-        ImageToIncrease.rectTransform.sizeDelta = new Vector2(ImageToIncrease.rectTransform.sizeDelta.x+ImageSize, ImageToIncrease.rectTransform.sizeDelta.y + ImageSize);
+        ImageToIncrease.rectTransform.localScale = new Vector2(ImageSize, ImageSize);
         return ImageToIncrease;
     }
 }
